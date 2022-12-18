e4e3e8b6da06fadab7c842d47dbf2827ae7dc3fd
hxl <2606710413@qq.com>
2022-12-18 19:46:48 +0800

    fix

--- app/src/main/java/com/hxl/android/xiaoan/word/net/AppApi.kt
+++ app/src/main/java/com/hxl/android/xiaoan/word/net/AppApi.kt
@@ -4,20 +4,12 @@ import com.hxl.android.xiaoan.word.bean.WordBean
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
-import retrofit2.http.GET
 import retrofit2.http.POST
 
 /**
  * 应用程序API
  */
 interface AppApi {
-    @GET("word/list")
-    fun getWordList(): Call<List<WordBean>>
-
-    @POST("word/add")
-    @FormUrlEncoded
-    fun addWord(@Field("word") word: String, @Field("mean") mean: String): Call<String>
-
     @POST("word/list")
     @FormUrlEncoded
     fun listWord(@Field("exclude") exclude: IntArray): Call<List<WordBean>>
